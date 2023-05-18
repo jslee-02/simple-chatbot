f392aabf183c4202de7eee18888af04e155e4713
Junseok Lee <123jellybean@snu.ac.kr>
2023-05-18 23:09:16 +0900

    Minor change

--- src/components/ChatLoader.js
+++ src/components/ChatLoader.js
@@ -4,7 +4,7 @@ export const ChatLoader = () => {
   return (
     <div className="flex flex-col flex-start">
       <div
-        className={`flex items-center bg-neutral-200 text-neutral-900 rounded-2xl px-4 py-2 w-fit`}
+        className={`flex items-center bg-neutral-200 text-neutral-900 rounded-xl px-4 py-2 w-fit`}
         style={{ overflowWrap: "anywhere" }}
       >
         <IconHourglassHigh className="animate-pulse" />
